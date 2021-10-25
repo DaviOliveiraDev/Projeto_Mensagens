0ad96ca5b009a3d1a154975dbb16b7ea72e3985a
dioogocosta <daviolirsoares@gmail.com>
2021-10-25 10:49:48 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 
 Route::group(['middleware' => ['auth']], function(){
